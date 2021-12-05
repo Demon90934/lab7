4447bc2793f8c6d6c3183961a1fb0a82d8dc3c8c
Demon90934 <91471287+Demon90934@users.noreply.github.com>
2021-12-05 15:56:24 +0500

    Add files via upload

---
+++ lab7-4.py
@@ -0,0 +1,28 @@
+﻿def inp():
+    n = int(input("Введите целое число n>=0:"))
+    m = int(input("Введите целое число 0<=m<=n:"))
+    return n, m
+
+
+def c(m, n):
+    if (m == 0) or (m == n):  # Если m равно 0 или m равно n, возвращаем 1
+        return 1
+    else:
+        return c(m, n - 1) + c(m - 1, n - 1)
+
+
+def out(res):
+    print(res)
+
+
+def out_wrong():
+    print("Ошибка! Некорректные значения")
+
+
+""" Основная программа """
+n, m = inp()
+if (n >= m) and (m >= 0):  # Проверяем условие 0 <= m <= n
+    res = c(m, n)
+    out(res)
+else:
+    out_wrong()
